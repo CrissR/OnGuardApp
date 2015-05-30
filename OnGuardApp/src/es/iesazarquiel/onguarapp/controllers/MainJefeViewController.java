bf1297c159c4ec11615e76a9b73be1ebd02f8f11
Jose Perez Moreno <jos.pe.mo@gmail.com>
2015-05-30 13:03:10 +0200

    Modificado MainJefeViewController (metodo VerFaltas funciona), cambio una linea que estaba mal en MainJefeView.fxml

--- OnGuardApp/src/es/iesazarquiel/onguarapp/controllers/MainJefeViewController.java
+++ OnGuardApp/src/es/iesazarquiel/onguarapp/controllers/MainJefeViewController.java
@@ -4,6 +4,7 @@ package es.iesazarquiel.onguarapp.controllers;
 import java.net.URL;
 import java.sql.Date;
 import java.sql.SQLException;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.ArrayList;
@@ -14,23 +15,49 @@ import es.iesazarquiel.onguarapp.models.dao.DaoDocente;
 import es.iesazarquiel.onguarapp.models.dao.DaoFaltas;
 import es.iesazarquiel.onguarapp.models.entidades.Docente;
 import es.iesazarquiel.onguarapp.models.entidades.Faltas;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Accordion;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.util.Callback;
 
+//esta clase es necesaria para meter cosas en el Tableview
+class LineaTabla {
+	
+	public SimpleStringProperty c1;
+	public SimpleStringProperty c2;
+	public SimpleStringProperty c3;
+	public SimpleStringProperty c4;
+	public SimpleStringProperty c5;
+	public SimpleStringProperty c6;
+	
+	public LineaTabla(String tc1, String tc2, String tc3, String tc4, String tc5, String tc6) {
+		this.c1 = new SimpleStringProperty(tc1);
+		this.c2 = new SimpleStringProperty(tc2);
+		this.c3 = new SimpleStringProperty(tc3);
+		this.c4 = new SimpleStringProperty(tc4);
+		this.c5 = new SimpleStringProperty(tc5);
+		this.c6 = new SimpleStringProperty(tc6);
+	}
+	
+	}
+
 public class MainJefeViewController {
 	
-	//dice al boton OK lo que tiene que hacer
+	//estos booleanos dicen al boton OK lo que tiene que hacer
 	private boolean activarVerHorarios = false;
 	private boolean activarComunicarFaltas = false;
 	private boolean activarVerFaltas = false;
@@ -107,26 +134,26 @@ public class MainJefeViewController {
     @FXML
     private Pane ContenedorSuperior;
 
-    @FXML
-    private TableView<String> tablaResultados;
+    @FXML  //la TableView es de tipo LineaTabla, la clase creada arriba
+    private TableView<LineaTabla> tablaResultados;
 
     @FXML
-    private TableColumn<String, String> columna1;
+    private TableColumn<LineaTabla, String> columna1;
 
     @FXML
-    private TableColumn<?, ?> columna2;
+    private TableColumn<LineaTabla, String> columna2;
 
     @FXML
-    private TableColumn<?, ?> columna3;
+    private TableColumn<LineaTabla, String> columna3;
 
     @FXML
-    private TableColumn<?, ?> columna4;
+    private TableColumn<LineaTabla, String> columna4;
 
     @FXML
-    private TableColumn<?, ?> columna5;
+    private TableColumn<LineaTabla, String> columna5;
 
     @FXML
-    private TableColumn<?, ?> columna6;
+    private TableColumn<LineaTabla, String> columna6;
 
     @FXML
     void initialize() throws SQLException {
@@ -183,7 +210,7 @@ public class MainJefeViewController {
     }
     
  
-    @FXML
+    @FXML //método que envía los elementos de la pantalla al fondo, para que no se muestren
     void enviarTodoAlFondo() {
     	botonok.toBack();
     	listaProfes.toBack();
@@ -193,10 +220,24 @@ public class MainJefeViewController {
     	fecha2.toBack();
     }
     
-    @FXML 
+    void ponerTodosLosBooleanosAFalso() {
+    	activarVerHorarios = false;
+    	activarComunicarFaltas = false;
+    	activarVerFaltas = false;
+    	activarVerGuardias = false;
+    	activarAsignarGuardias = false;
+    	activarFirmarGuardia = false;
+        activarGenerarInformeGuardias = false;
+        activarGenerarInformeFaltas = false;
+    }
+    
+    @FXML //los siguientes métodos controlan los botones de la izquierda. 
+    //Dicen qué elementos tienen que aparecer en la pantalla central.
+  
     void botonVerHorarios() {
     	
     	enviarTodoAlFondo();
+    	ponerTodosLosBooleanosAFalso();
     	
     	botonok.toFront();
     	listaProfes.toFront();
@@ -207,6 +248,7 @@ public class MainJefeViewController {
     void botonComunicarFalta() {
     	
     	enviarTodoAlFondo();
+    	ponerTodosLosBooleanosAFalso();
     	
     	botonok.toFront();
     	listaProfes.toFront();
@@ -218,6 +260,7 @@ public class MainJefeViewController {
     @FXML
     void botonVerFaltas() {
     	enviarTodoAlFondo();
+    	ponerTodosLosBooleanosAFalso();
     	
     	botonok.toFront();
     	listaProfes.toFront();
@@ -230,6 +273,7 @@ public class MainJefeViewController {
     @FXML
     void botonVerGuardias() {
     	enviarTodoAlFondo();
+    	ponerTodosLosBooleanosAFalso();
     	
     	botonok.toFront();
     	listaProfes.toFront();
@@ -242,6 +286,7 @@ public class MainJefeViewController {
     @FXML
     void botonAsignarGuardia() {
     	enviarTodoAlFondo();
+    	ponerTodosLosBooleanosAFalso();
     	
         botonok.toFront();
         listaFaltas.toFront();
@@ -253,6 +298,7 @@ public class MainJefeViewController {
     @FXML
     void botonFirmarGuardia() {
     	enviarTodoAlFondo();
+    	ponerTodosLosBooleanosAFalso();
     	
         botonok.toFront();
         fecha1.toFront();
@@ -265,6 +311,7 @@ public class MainJefeViewController {
     @FXML
     void botonGenerarInformeGuardias() {
     	enviarTodoAlFondo();
+    	ponerTodosLosBooleanosAFalso();
     	
     	botonok.toFront();
     	fecha1.toFront();
@@ -275,6 +322,7 @@ public class MainJefeViewController {
     @FXML
     void botonGenerarInformeFaltas() {
     	enviarTodoAlFondo();
+    	ponerTodosLosBooleanosAFalso();
     	
     	botonok.toFront();
     	fecha1.toFront();
@@ -283,84 +331,180 @@ public class MainJefeViewController {
     }
     
     
-    @FXML
+    @FXML  //Método para mostrar la información visual en pantalla
     void mostrarResultados() throws SQLException {
     	
     	if (activarVerHorarios) {
     		
-    		/* Mï¿½todo de Alejandro*/
+    		/* Parte de Alejandro*/
     		
-    		activarVerHorarios = false;
+    		
     	}
     	
     	else if (activarComunicarFaltas) {
     		
-    		/*Mï¿½todo de Cristina*/
+    		/*Parte de Cristina, requiere crear un objeto de la clase Falta*/
     		
-    		activarComunicarFaltas = false;
+    		
     	}
     	
     	else if (activarVerFaltas) {
     		
-    		/*Mï¿½todo de Carlos*/
-    		DaoDocente daodocente = new DaoDocente();
+    		/*Primer metodo en comun, con ayuda de Angel*/
     		
+    		//Ponemos encabezados a las columnas
+    		columna1.setText("Fecha falta");
+    		columna2.setText("Hora falta");
+      		columna3.setText("Profesor de guardia");
+      		columna4.setText(" ");
+      		columna5.setText(" ");
+      		columna6.setText(" ");
+    	
+      		//cogemos el nombre de profesor, averiguamos su codigo
+    		DaoDocente daodocente = new DaoDocente();
     		String nombreProfe = (String) listaProfes.getSelectionModel().getSelectedItem().toString();//captura el profesor
     		String codProfe = daodocente.encontrarCodigo(nombreProfe);
     		
+    		//cogemos la fecha de inicio del DatePicker y la convertimos en Date sql
     		LocalDate fechaInicio= fecha1.getValue();
-    		Date fechaInicio2 = (Date) Date.from(fechaInicio.atStartOfDay(ZoneId.systemDefault()).toInstant());//coge la fecha
-    		LocalDate fechaFin= fecha2.getValue();
-    		Date fechaFin2 = (Date) Date.from(fechaFin.atStartOfDay(ZoneId.systemDefault()).toInstant());//coge la fecha
-    	    // System.out.println(fechaInicio2 + " " + fechaFin2 + " " + codProfe);
+    		Instant instant = Instant.from(fechaInicio.atStartOfDay(ZoneId.systemDefault()));
+    		java.util.Date theDate = Date.from(instant);
+    		java.sql.Date fechaInicio2 = new java.sql.Date(theDate.getTime());
     		
-    		//insertamos los datos de arriba//	
-    		DaoFaltas daofaltas = new DaoFaltas();//necesario para usar el mÃ©todo verFaltasProf
+    		//cogemos la fecha de fin del DatePicker y la convertimos en Date sql
+    		LocalDate fechaFin = fecha2.getValue();
+    		Instant instant2 = Instant.from(fechaFin.atStartOfDay(ZoneId.systemDefault()));
+    		java.util.Date theDate2 = Date.from(instant2);
+    		java.sql.Date fechaFin2 = new java.sql.Date(theDate2.getTime());  	
+    		 		
+    		//insertamos los datos de arriba en un arraylist que llevará los datos de las faltas solicitadas
+    		DaoFaltas daofaltas = new DaoFaltas();//necesario para usar el metodo verFaltasProf
     		ArrayList<String> lasFaltas = daofaltas.verFaltasProf(codProfe, fechaInicio2, fechaFin2);
-    	
-    		ObservableList<String> listaFaltas = FXCollections.observableArrayList(lasFaltas);
-    		//columna1.setCellFactory(new Callback<TableColumn.CellDataFeatures<ObservableList<String>,String>,ObservableValue<String>>);
-    		columna1.setCellValueFactory(new PropertyValueFactory<String, String>("cosas"));
-    		tablaResultados.getItems().setAll(listaFaltas);
     		
-    		activarVerFaltas = false;
+    		//creamos las lineas de la tabla en pantalla (clase creada arriba del todo)
+    		ArrayList <LineaTabla> lineasTablaVerFaltas = new ArrayList<LineaTabla>();
+    		
+    		//ponemos los valores en las tres columnas que me interesan según el arrayList de lasFaltas
+    		//las columnas no utilizadas de las seis las rellenamos con vacíos
+    		for (int i=0; i<lasFaltas.size(); i=i+4) {
+    			LineaTabla linea = new LineaTabla(lasFaltas.get(i+1), lasFaltas.get(i+2), lasFaltas.get(i+3),"","","");
+    			lineasTablaVerFaltas.add(linea);
+    		}
+  
+    		//asignamos los valores de las "columnas" de LineaTabla a cada columna del TableView, del siguiente modo
+    		columna1.setCellValueFactory(cellData -> cellData.getValue().c1); 
+    		columna2.setCellValueFactory(cellData -> cellData.getValue().c2);
+    		columna3.setCellValueFactory(cellData -> cellData.getValue().c3);
+    		
+    		//decimos a la tabla que coja los valores del arraylist de lineas de la tabla   
+    		tablaResultados.getItems().setAll(lineasTablaVerFaltas);
+    	
     		
     	}
     	
     	else if (activarVerGuardias) {
     		
-    		/*Mï¿½todo de Carlos*/
+    		/*Parte de Carlos*/
     		
-    		activarVerGuardias = false;
+    		
     	}
     	
     	else if (activarAsignarGuardias) {
     		
-    		/*Mï¿½todo de Cristina*/
+    		/*Parte de Cristina*/
     		
-    		activarAsignarGuardias = false;
+    		
     		
     	}
     	
     	else if (activarFirmarGuardia) {
     		
-    		/*Mï¿½todo de Ana*/
+    		/*Parte de Ana*/
     		
-    		activarFirmarGuardia = false;
+    		
     	}
     	
     	else if (activarGenerarInformeGuardias) {
     		
-    		/*Mï¿½todo de Jose*/
+    		/*Parte de Alejandro*/
     		
-    		activarGenerarInformeGuardias = false;
+    		
     	}
     	
     	else if (activarGenerarInformeFaltas) {
     		
-    		/*Mï¿½todo de Jose*/
+    		/*Metodo de Jose*/
     		
-    		activarGenerarInformeFaltas = false;
+    		columna1.setText("HORA");
+    		columna2.setText("B-301");
+      		columna3.setText("B-302");
+      		columna4.setText(" ");
+      		columna5.setText(" ");
+      		columna6.setText(" ");
+    	
+      		
+    		//coger la fecha del DatePicker y la convertimos en Date sql
+    		LocalDate fecha = fecha1.getValue();
+    		Instant instant = Instant.from(fecha.atStartOfDay(ZoneId.systemDefault()));
+    		java.util.Date laFecha = Date.from(instant);
+    		java.sql.Date fecha2 = new java.sql.Date(laFecha.getTime());
+    		
+    		//insertamos los datos de arriba en un arraylist que llevará los datos de las faltas solicitadas
+    		DaoFaltas daofaltas = new DaoFaltas();//necesario para usar el metodo verFaltasProf
+    		ArrayList<String> lasFaltas = daofaltas.generarInformeFaltasCub(fecha2);
+    		
+    		//creamos las lineas de la tabla en pantalla (clase creada arriba del todo)
+    		ArrayList <LineaTabla> lineasTablaGenerarInformeFaltas = new ArrayList<LineaTabla>();
+    		
+    		/* Devuelve algo así como:
+    		hora5
+    		B-302
+    		profe004
+    		hora6
+    		B-302
+    		profe004
+    		*/
+    		
+    		//creo seis lineas de tabla, una para cada hora
+    		for (int i=0; i<6; i++) {
+    			LineaTabla linea = new LineaTabla("hora"+(i+1),"","","","","");
+    			lineasTablaGenerarInformeFaltas.add(linea);
+    		}
+    		
+    		//miro las horas que me ha devuelto el arrayList anterior, y meto el profe en la columna del aula correspondiente
+    		for (int i=0; i < lasFaltas.size(); i=i+3) {
+    			//profesor
+    			DaoDocente daodocente = new DaoDocente();
+    			String nombreProf = daodocente.encontrarNombre(lasFaltas.get(i+1));
+    			
+    			int numeroLinea = -1;
+    			if (lasFaltas.get(i).equals("hora1")) numeroLinea = 0;
+    			if (lasFaltas.get(i).equals("hora2")) numeroLinea = 1;
+    			if (lasFaltas.get(i).equals("hora3")) numeroLinea = 2;
+    			if (lasFaltas.get(i).equals("hora4")) numeroLinea = 3;
+    			if (lasFaltas.get(i).equals("hora5")) numeroLinea = 4;
+    			if (lasFaltas.get(i).equals("hora6")) numeroLinea = 5;
+    			
+    			
+    			
+    				if (lasFaltas.get(i+2).equals("B-301")) {
+    					lineasTablaGenerarInformeFaltas.get(numeroLinea).c1 = new SimpleStringProperty(nombreProf);
+    				}
+    				else if (lasFaltas.get(i+2).equals("B-302")) {
+    					lineasTablaGenerarInformeFaltas.get(numeroLinea).c2 = new SimpleStringProperty(nombreProf);
+    				}
+    			
+    			
+    		}
+    		    		  		 
+    		//asignamos los valores de las "columnas" de LineaTabla a cada columna del TableView
+    		columna1.setCellValueFactory(cellData -> cellData.getValue().c1); 
+    		columna2.setCellValueFactory(cellData -> cellData.getValue().c2);
+    	    		
+    		//decimos a la tabla que coja los valores del arraylist de lineas de la tabla   
+    		tablaResultados.getItems().setAll(lineasTablaGenerarInformeFaltas);
+    		
+    		
     		
     	}
     
